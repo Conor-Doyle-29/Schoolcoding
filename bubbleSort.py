ebbf8895e78b8e427e2e720e1bca0e1ec620ff18
Conor Doyle <18cldoyle@stbenilduscollege.com>
2023-09-25 14:24:09 +0100

    IDFK

---
+++ bubbleSort.py
@@ -0,0 +1,9 @@
+myList = [85,85, 24, 63, 45, 17, 31, 96, 50]
+for outerIndex in range(len(myList)-1):
+    print(myList)
+    for index in range(len(myList)-1):
+        if myList[index]>myList[index+1]:
+            tempValue = myList[index]
+            myList[index]=myList[index+1]
+            myList[index+1]=tempValue
+print(myList)
--- insertionsorting.py
+++ insertionsorting.py
@@ -1,4 +1,4 @@
-myList = [85, 24, 63, 45, 17, 31, 96, 50]
+myList = [85,85, 24, 63, 45, 17, 31, 96, 50]
 for index in range (1, len(myList)):
   print(myList)
   itemInsert = myList[index]
